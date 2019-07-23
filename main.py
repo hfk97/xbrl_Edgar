ff6514f26a70fe0af5d2329bd0dbee80e68b45a9
Felix Kruse <Felix@MacBook-Pro-43.local>
2019-07-23 22:48:16 +0200

    added xbrl_parser file

--- main.py
+++ main.py
@@ -13,3 +13,26 @@ def getpack(package):
         subprocess.call([sys.executable, "-m", "pip", "install", package])
         return (importlib.import_module(package))
         # import package
+
+
+
+bs4=getpack("bs4")
+from bs4 import BeautifulSoup
+urllib=getpack("urllib")
+request=getpack("urllib.request")
+re=getpack("re")
+from urllib.request import Request
+
+
+def make_soup(url):
+    page = open(url)
+    soup = BeautifulSoup(page.read(), 'html.parser')
+    return soup
+
+
+soup=make_soup("/Users/Felix/Desktop/table.html")
+
+soup.findAll('p')
+
+
+
